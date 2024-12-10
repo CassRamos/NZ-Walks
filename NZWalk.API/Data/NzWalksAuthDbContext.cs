ae52109f1c8a0cc0411098c08d0e95b88040ede2
Cass <cassianoleaoramos@gmail.com>
2024-12-09 21:55:00 -0300

    Create auth database

--- NZWalk.API/Data/NzWalksAuthDbContext.cs
+++ NZWalk.API/Data/NzWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalk.API.Data
@@ -7,5 +8,33 @@ namespace NZWalk.API.Data
     {
         public NzWalksAuthDbContext(DbContextOptions<NzWalksAuthDbContext> options) : base(options) 
         { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "17d9079f-6ed4-4a6a-b3ec-8e3246e80efd";
+            var writerRoleId = "92dbaf8e-b494-4380-a02d-2218564efd1c";
+
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }

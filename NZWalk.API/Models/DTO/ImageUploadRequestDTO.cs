8244a52d6188218e5d9a7df330cb103e7ae8882d
Cass <cassianoleaoramos@gmail.com>
2024-12-12 22:54:04 -0300

    Add Images table to NzWalksDbContext via migration

---
+++ NZWalk.API/Models/DTO/ImageUploadRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalk.API.Models.DTO
+{
+    public class ImageUploadRequestDTO
+    {
+        [Required]
+        public IFormFile File { get; set; }
+        [Required]
+        public string Filename { get; set; }
+        public string? FileDescription { get; set; }
+    }
+}
